846c71c0449b30c57ad5243a00eca1dded6c2dc6
parazitol-official <73265222+parazitol-official@users.noreply.github.com>
2020-10-26 03:17:11 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,247 @@
+var new_lang = {
+
+    "lt1": "UN COLPO DEVASTANTE AI PARASSITI",
+
+"lt2": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Normalizza il lavoro del tratto gastro-intestinale",
+
+"lt3": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Elimina tutte le specie di elminti",
+
+"lt4": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Elimina i prodotti dell'attività vitale dei parassiti",
+
+"lt5": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: visible; animation-iteration-count: 5; animation-name: pulse;\"></i> Impedisce la ricomparsa d'infestazioni",
+
+"lt6": "OFFERTA<br> SPECIALE",
+
+"lt7": "al termine della promozione è rimasto",
+
+"lt8": "<div class=\"price_old\"></div>  Prezzo normale",
+
+"lt9": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: visible; animation-delay: 2s; animation-iteration-count: 3; animation-name: pulse;\"></div>  Prezzo promozionale",
+
+"lt10": "Ordinare",
+
+"lt11": "SECONDO GLI ULTIMI DATI DELLA WORLD HEALTH ORGANIZATION,<br> PIÚ DI 1 MILIARDO DI PERSONE È INFETTO DA PARASSITI",
+
+"lt12": "Il 90% non sospetta nemmeno di ospitare elminti nel proprio organismo",
+
+"lt13": "SIETE CERTI DI NON TROVARVI IN QUESTA CATEGORIA? CONTROLLATE I SINTOMI DELL'INFESTAZIONE E LO SAPRETE!",
+
+"lt14": "SINTOMI DELL'ELMINTOSI",
+
+"lt15": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Senso di affaticamento",
+
+"lt16": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Mal di testa, malori",
+
+"lt17": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Infiammazione delle vie aeree superiori",
+
+"lt18": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Irritabilità, aggressività",
+
+"lt19": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Eruzioni cutanee varie",
+
+"lt20": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Perdita di peso, anche in caso di appetito regolare",
+
+"lt21": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Problemi di sonno",
+
+"lt22": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Manifestazioni allergiche (occhi lacrimanti, eruzioni cutanee, mucosa nasale irritata)",
+
+"lt23": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Frequenti raffreddori, angina, naso chiuso",
+
+"lt24": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Dolori articolari e muscolari",
+
+"lt25": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Ingrossamento dei nodi linfatici",
+
+"lt26": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Disfunzioni digestive: gonfiore, costipazione o diarrea",
+
+"lt27": "CANALI INFETTIVI DELL'ELMINTOSI",
+
+"lt28": "Verdure, frutti e bacche mal puliti",
+
+"lt29": "Carne e pesce cotti in maniera insufficiente",
+
+"lt30": "Acqua o terreno infetti",
+
+"lt31": "Contatto con animali domestici",
+
+"lt32": "Luoghi e trasporti pubblici",
+
+"lt33": "Punture d'insetti",
+
+"lt34": "Ristoranti e fast-food",
+
+"lt35": "COMINCIATE A CURARVI ADESSO!",
+
+"lt36": "COMINCIARE",
+
+"lt37": "Parassiti,<br> che causano gravi complicazioni:",
+
+"lt38": "CISTICERCO",
+
+"lt39": "Nella maggior parte dei casi (più del 60%) il cisticerco infesta la massa cerebrale, più raramente –i muscoli scheletrici e gli occhi. Vive dai 5 ai 30 anni, causa<span> tumori</span> ",
+
+"lt40": "OSSIURI",
+
+"lt41": "Vivono e si moltiplicano negli organi digerenti. Portano alla formazione di <span>ulcere</span>  e<span> ad emorragie estese</span> ",
+
+"lt42": "ANCHILOSTOMI",
+
+"lt43": "Danneggiano la mucosa e si nutrono del sangue che ne viene rilasciato. Lasciano ferite sanguinanti, che determinano lo sviluppo di <span>anemia grave</span>  e <span>setticemia</span> ",
+
+"lt44": "ALVEOCOCCO",
+
+"lt45": "Si verifica la necrosi del tessuto dell'organo colpito. Inoltre, vengono gravemente danneggiati e<span> cessano di svolgere debitamente le proprie funzioni i vasi sanguigni</span> ",
+
+"lt46": "AZIONE SU PIÚ FRONTI <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+
+"lt47": "ELIMINA",
+
+"lt48": "tutti i possibili tipi d'infestazione in tempo brevissimo",
+
+"lt49": "ESPELLE",
+
+"lt50": "dall'organismo i prodotti dell'attività fisiologica dei parassiti",
+
+"lt51": "FAVORISCE",
+
+"lt52": "la disintossicazione dell'organismo",
+
+"lt53": "RICOSTITUISCE",
+
+"lt54": "le funzioni del sistema immunitario, endocrino, digerente, nervoso, cardio-vascolare",
+
+"lt55": "PROTEGGE",
+
+"lt56": "da future infestazioni, forma un ambiente patogeno nell'organismo e rafforza l'immunità",
+
+"lt57": "ORDINARE",
+
+"lt58": "UNA FORTE COMPOSIZIONE NATURALE <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+
+"lt59": "MORINGA",
+
+"lt60": "Grazie ad una composizione unica, disintossica l'organismo da sostanze dannose e dai metalli pesanti. Combatte efficacemente i parassiti gastrointestinali, colpendo il DNA estraneo a quello dell'organismo umano. Purifica l'intestino e le vie biliari",
+
+"lt61": "PAU D'ARCO",
+
+"lt62": "Il lapacolo (un composto specifico, presente nella corteccia) impedisce il moltiplicarsi di microrganismi patogeni, inibisce la riproduzione e lo sviluppo dei parassiti all'interno delle cellule.",
+
+"lt63": "NOCE NERA",
+
+"lt64": "Gli elementi della noce nera interrompono il metabolismo dei vermi, ne causano la paralisi del tessuto muscolare, aumentano poi l'immunità e le funzioni protettive del corpo, purificano il sangue dalle tossine.",
+
+"lt65": "FOGLIE D'ULIVO",
+
+"lt66": "Contiene oleuropeina, che ha effetti antiparassitari e antifungini, riducendo il danno ossidativo nel sangue, nel fegato, nei reni e nei tessuti cerebrali.",
+
+"lt67": "RADICE DI CURCUMA",
+
+"lt68": "Nella radice della curcuma troviamo cineolo, curcumina e vitamina P. Il cineolo normalizza il funzionamento degli organi digestivi e inibisce l'attività vitale dei vermi, l'amerone ha un effetto antiparassitario contro i nematodi, la vitamina P ripristina il funzionamento degli organi che formano il sangue, purificando inoltre lo stesso.",
+
+"lt69": "ASAFETIDA",
+
+"lt70": "È celebre grazie alle potentissime proprietà battericide ed antimicrobiche. Ne viene particolarmente apprezzata la capacità di espellere i cestodi dal tratto gastro-intestinale.",
+
+"lt71": "IN COSA<span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> IL PREPARATO È MEGLIO DEGLI ALTRI?",
+
+"lt72": "PREPARATI CHIMICI",
+
+"lt74": "PREPARATI CHIMICI",
+
+"lt75": "Presenza di sostanze chimiche <br>dannose <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt76": "Influenza negativa sugli organi e i sistemi interni<br> <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt77": "Favoriscono<br> una riduzione dell'immunità <i class=\"wow pulse minus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt78": "Danneggiano gli organi del tratto gastro-intestinale:<br> <span> fegato, stomaco, pancreas, intestino. Provocano<br> gastriti ed ulcere.</span> ",
+
+"lt80": "Una composizione completamente<br> naturale <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt81": "Non ha effetti <br>collaterali, protegge<br> gli organi interni <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt82": "Potenzia l'immunità<br> e la resistenza<br> dell'organismo alle infezioni <i class=\"wow pulse plus\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> ",
+
+"lt83": "Esercita un'influenza positiva sul tratto gastro-intestinale:<br> <span>, migliora la secrezione biliare, stimola le funzioni intestinali</span> ",
+
+"lt84": " <span>Parazitol</span> DELICATO CON L'ESSERE UMANO, SPIETATO CON GLI ELMINTI",
+
+"lt85": "La maggior parte della gente non sospetta nemmeno che un organismo umano su due è INFESTATO DA PARASSITI.",
+
+"lt86": "Nel 95% dei casi la presenza degli elminti non causa sintomi di sorta, almeno fino a quando non abbiano causato danni irreparabili agli organi interni e non abbiano determinato disfunzioni in tutti i sistemi dell'organismo.",
+
+"lt87": "Ad oggi, solo Parazitol è in grado di liberare COMPLETAMENTE l'organismo da più di 250 tipi di parassiti. Il preparato crea nell'organismo umano un ambiente che distrugge l'intero processo che porta alla formazione e alla riproduzione dei parassiti.",
+
+"lt88": "L'esperienza dei miei pazienti conferma che Parazitol è il preparato più efficiente e sicuro per eliminare ed espellere TUTTI i tipi di infestazione parassitaria, incluse le uova. Quest'ultima capacità elimina ogni possibile ricomparsa dei parassiti.",
+
+"lt89": "Il successo della cura è garantito nel 100% dei casi».",
+
+"lt90": "Michele Vasari, parassitologo",
+
+"lt91": "AZIONE COMPLEMENTARE <span class=\"fadeIn wow\" style=\"visibility: hidden; animation-name: none;\">Parazitol</span> ",
+
+"lt92": "Favorisce la purificazione di organi e tessuti, neutralizza l'intossicazione dovuta all'infezione parassitaria",
+
+"lt93": "Rigenera le mucose, purifica il sangue, la linfa e i liquidi intercellulari",
+
+"lt94": "Normalizza l'equilibrio di ossidazione, la cui mancata ristabilizzazione è foriera di formazioni oncologiche",
+
+"lt95": "Normalizza l'equilibrio di ossidazione, la cui mancata ristabilizzazione è foriera di formazioni oncologiche",
+
+"lt96": "Favorisce la cura di ulcere e gastriti, annientando i batteri che ne causano la comparsa",
+
+"lt97": "Ripristina e normalizza le funzioni degli organi intaccati",
+
+"lt98": "Influenza positivamente gli organi del tratto gastro-intestinale",
+
+"lt99": "Migliora lo stato della pelle, delle unghie, dei capelli",
+
+"lt100": "ORDINARE",
+
+"lt101": "COMMENTI DEI CLIENTI",
+
+"lt102": "<div class=\"slide-description-title\">Elena, Milano </div> «Questo preparato mi è piaciuto. L'abbiamo provato io e mio marito. Sia io che lui abbiamo avvertito diversi effetti: sudore freddo, diarrea, eruzioni cutanee ed un forte mal di testa. È passato però tutto dopo una settimana e mezzo, ora ci sentiamo benissimo! Parazitol agisce in maniera delicata, ma certa».",
+
+"lt103": "<div class=\"slide-description-title\">Lucia, Macerata </div> «Sono stata male per due settimane, ho fatto le analisi ed è venuto fuori che avevo i parassiti. Ho preso la medicina che mi aveva prescritto il medico, ma mi sono solo sentita peggio ogni giorno che passava. Mica so che mi sarebbe successo, se non fossi venuta a sapere di questo nuovo preparato. Mi ha salvato, non scherzo per niente. Un mese dopo ho rifatto le analisi, ed ero pulita. Adesso voglio prendere Parazitol una volta all'anno per profilassi».",
+
+"lt104": "<div class=\"slide-description-title\">Andrea, Ancona </div>«Il gatto che abbiamo non è casalingo, gli piace andarsene in giro. È così che ci ha portato gli elminti in casa. Io non ho avuto problemi particolari, mentre mia moglie, poverina, ha dovuto passare certi tormenti... Prima le è cominciato il bruxismo notturno, poi ha iniziato ad avere la febbre ed attacchi di vomito. Ci siamo curati insieme, perché quasi certamente aveva contagiato anche me. Abbiamo preso Parazitol e ci è passato tutto».",
+
+"lt110": "UN COLPO DEVASTANTE AI PARASSITI",
+
+"lt111": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Normalizza il lavoro del tratto gastro-intestinale",
+
+"lt112": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Elimina tutte le specie di elminti",
+
+"lt113": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Elimina i prodotti dell'attività vitale dei parassiti",
+
+"lt114": " <i class=\"wow pulse\" data-wow-iteration=\"5\" style=\"visibility: hidden; animation-iteration-count: 5; animation-name: none;\"></i> Impedisce la ricomparsa d'infestazioni",
+
+"lt115": "OFFERTA<br> SPECIALE",
+
+"lt116": "al termine della promozione è rimasto",
+
+"lt117": "<div class=\"price_old\"></div>  Prezzo normale",
+
+"lt118": "<div class=\"price_main wow pulse\" data-wow-delay=\"2s\" data-wow-iteration=\"3\" style=\"visibility: hidden; animation-delay: 2s; animation-iteration-count: 3; animation-name: none;\"></div>  Prezzo promozionale",
+
+"lt119": "Ordinare",
+
+"lt120": "Come operiamo",
+
+"lt121": "Lasciare una richiesta d'ordine sul nostro sito",
+
+"lt122": "Un operatore vi contatterà per confermare l'ordine",
+
+"lt123": "Pagamento alla ricezione dell'ordine"
+
+    
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
